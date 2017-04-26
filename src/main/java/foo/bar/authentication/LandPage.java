a29748c7b7189fc3f1b8a6008a2d8b9781041e4d
Mehr5 <kirils.gvozdevs@gmail.com>
2017-04-26 17:32:41 +0300

    New files

---
+++ src/main/java/foo/bar/authentication/LandPage.java
@@ -0,0 +1,17 @@
+package foo.bar.authentication;
+
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+
+public class LandPage extends WebPage {
+
+    private static final long serialVersionUID = 1L;
+
+    public LandPage(final PageParameters parameters) {
+        // Add the simplest type of label
+       // add(new Label("message", "If you see this message wicket is properly configured and running"));
+
+    }
+}
